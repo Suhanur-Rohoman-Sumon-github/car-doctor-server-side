4ff49e3e1b730b2bc4551071e20746f8c854a62c
su44248124 <sumonsuhanurrohoman@gmail.com>
2023-05-15 03:31:33 +0600

    patch added

--- index.js
+++ index.js
@@ -25,36 +25,55 @@ async function run() {
         await client.connect();
         // Send a ping to confirm a successful connection
         const carDoctor = client.db('carDoctor').collection('carServises')
-        const carDoctors =  client.db('carDoctor').collection('bookings')
+        const carDoctors = client.db('carDoctor').collection('bookings')
 
-        app.get('/servises',async(req,res)=>{
+        app.get('/servises', async (req, res) => {
             const cursor = carDoctor.find()
             const result = await cursor.toArray();
             res.send(result)
         })
-        app.get('/servises/:id',async(req,res)=>{
+        app.get('/servises/:id', async (req, res) => {
             const id = req.params.id
-            const query = {_id : new ObjectId(id)}
+            const query = { _id: new ObjectId(id) }
             const options = {
                 // Include only the `title` and `imdb` fields in the returned document
-                projection: { _id: 0, title: 1, price:1,service_id:1,img:1 },
-              };
-            const result = await carDoctor.findOne(query,options)
+                projection: { _id: 0, title: 1, price: 1, service_id: 1, img: 1 },
+            };
+            const result = await carDoctor.findOne(query, options)
             res.send(result)
         })
-        app.get('/bookings',async(req,res)=>{
+        app.get('/bookings', async (req, res) => {
             let query = {};
-            if(req.query?.email){
-                query = {name: req.query.email}
+            if (req.query?.email) {
+                query = { name: req.query.email }
             }
             const cursor = carDoctors.find(query)
             const result = await cursor.toArray()
             res.send(result)
         })
-        app.post('/bookings',async(req,res)=>{
+        app.post('/bookings', async (req, res) => {
             const bookings = req.body
             const result = await carDoctors.insertOne(bookings)
-            console.log(result)
+            res.send(result)
+        })
+        app.patch('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const updateBooking = req.body
+            const updateDoc = {
+                $set: {
+                    status : updateBooking.status
+                },
+                
+            };
+            const result = await carDoctors.updateOne(query,updateDoc)
+            res.send(result)
+            
+        })
+        app.delete('/bookings/:id', async (req, res) => {
+            const id = req.params.id
+            const query = { _id: new ObjectId(id) }
+            const result = await carDoctors.deleteOne(query)
             res.send(result)
         })
 
